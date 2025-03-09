8d1e977598fec640912c7fdbb5894da8e26ae33e
Anjan Roy <hello@itzmeanjan.in>
2025-03-09 13:38:31 +0400

    Update server to handle setup request from client
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- src/server.rs
+++ src/server.rs
@@ -1,32 +1,80 @@
+use chalamet_pir::server::Server;
+use rand::prelude::*;
+use rand_chacha::ChaCha8Rng;
+use serde::{Deserialize, Serialize};
+use serde_json::{Map, Value};
+use std::collections::HashMap;
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
 use std::time::Instant;
-
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 
+#[derive(Clone, Serialize, Deserialize, Debug)]
+struct ClientSetupParams {
+    seed: [u8; chalamet_pir::SEED_BYTE_LEN],
+    hint: Vec<u8>,
+    filter: Vec<u8>,
+}
+
 /// Handles an individual client connection.
-async fn handle_client(mut stream: TcpStream) {
+async fn handle_client(mut stream: TcpStream, setup_params: ClientSetupParams) {
     println!("New connection from: {}", stream.peer_addr().unwrap());
-    let mut buf = [0u8; 512];
+    let mut buf = vec![0u8; 1024 * 1024];
 
     loop {
         match stream.read(&mut buf).await {
             Ok(0) => {
-                // Connection closed
                 println!("Connection closed");
                 break;
             }
             Ok(n) => {
-                // Echo the message back to the client
-                let received = String::from_utf8_lossy(&buf[..n]);
-                println!("Received: {}", received);
+                match n {
+                    5 => {
+                        let received = String::from_utf8_lossy(&buf[..n]);
+                        if received.to_ascii_lowercase() == "setup" {
+                            let setup_params_bytes = serde_json::to_vec(&setup_params).unwrap();
+                            println!("total = {}", setup_params_bytes.len());
 
-                let start_tm = Instant::now();
-                if let Err(e) = stream.write_all(&buf[..n]).await {
-                    eprintln!("Failed to write to client: {}", e);
-                    break;
-                } else {
-                    println!("Responded in {:?}", start_tm.elapsed());
-                }
+                            stream
+                                .write_u64_le(setup_params_bytes.len() as u64)
+                                .await
+                                .unwrap_or_else(|e| {
+                                    eprintln!(
+                                        "Failed to send setup parameters metadata to PIR client: {}",
+                                        e
+                                    );
+                                });
+
+                            stream
+                                .write_all(&setup_params_bytes)
+                                .await
+                                .unwrap_or_else(|e| {
+                                    eprintln!(
+                                        "Failed to send setup parameters to PIR client: {}",
+                                        e
+                                    );
+                                });
+                        } else {
+                            stream
+                                .write_all(b"unknown request")
+                                .await
+                                .unwrap_or_else(|e| {
+                                    eprintln!("Failed to inform client: {}", e);
+                                });
+                        }
+                    }
+                    _ => {
+                        let start_tm = Instant::now();
+                        if let Err(e) = stream.write_all(&buf[..n]).await {
+                            eprintln!("Failed to write to client: {}", e);
+                            break;
+                        } else {
+                            println!("Responded in {:?}", start_tm.elapsed());
+                        }
+                    }
+                };
             }
             Err(e) => {
                 eprintln!("Failed to read from client: {}", e);
@@ -38,6 +86,53 @@ async fn handle_client(mut stream: TcpStream) {
 
 #[tokio::main]
 async fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <path_to_json_file>", args[0]);
+        std::process::exit(1);
+    }
+    let file_path = &args[1];
+
+    let file = File::open(file_path).unwrap_or_else(|err| {
+        eprintln!("Error opening file {}: {}", file_path, err);
+        std::process::exit(1);
+    });
+    let reader = BufReader::new(file);
+
+    let deserialized: Map<String, Value> = serde_json::from_reader(reader).unwrap_or_else(|err| {
+        eprintln!("Error parsing JSON: {}", err);
+        std::process::exit(1);
+    });
+
+    println!("Parsing JSON file");
+
+    let start_tm = Instant::now();
+    let kv_map: HashMap<Vec<u8>, Vec<u8>> = deserialized
+        .into_iter()
+        .map(|(k, v)| (k.as_bytes().to_vec(), v.to_string().as_bytes().to_vec()))
+        .collect();
+    let kv_map_ref = kv_map
+        .iter()
+        .map(|(k, v)| (k.as_slice(), v.as_slice()))
+        .collect();
+
+    println!("Done in {:?}", start_tm.elapsed());
+
+    let mut rng = ChaCha8Rng::from_os_rng();
+    let mut seed_μ = [0u8; chalamet_pir::SEED_BYTE_LEN]; // You'll want to generate a cryptographically secure random seed
+    rng.fill_bytes(&mut seed_μ);
+
+    println!("Setting up ChalametPIR server");
+
+    let start_tm = Instant::now();
+    let (server, hint_bytes, filter_param_bytes) = Server::setup::<3>(&seed_μ, kv_map_ref)
+        .unwrap_or_else(|e| {
+            eprintln!("Server setup failed: {}", e);
+            std::process::exit(1);
+        });
+
+    println!("Done in {:?}", start_tm.elapsed());
+
     // Bind the TCP listener to an address.
     let listener = TcpListener::bind("127.0.0.1:7878")
         .await
@@ -45,13 +140,20 @@ async fn main() {
 
     println!("Server listening on 127.0.0.1:7878");
 
+    let client_setup_params = ClientSetupParams {
+        seed: seed_μ,
+        hint: hint_bytes,
+        filter: filter_param_bytes,
+    };
+
     // Accept incoming connections in an infinite loop.
     loop {
         match listener.accept().await {
             Ok((stream, _addr)) => {
-                // Spawn an asynchronous task for each client connection.
+                let movable_client_setup_params = client_setup_params.clone();
+
                 tokio::spawn(async move {
-                    handle_client(stream).await;
+                    handle_client(stream, movable_client_setup_params).await;
                 });
             }
             Err(e) => {
